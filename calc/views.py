0eb8fd487bf951740936f9646dea5eeddb29871f
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-07-08 11:32:40 +0600

    Adding result File and Perform 'GET' Operation

--- calc/views.py
+++ calc/views.py
@@ -5,3 +5,10 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name':'M. A. MAMUN'})
+
+
+def result(request):
+    var1 = int(request.GET['num1'])
+    var2 = int(request.GET['num2'])
+    result = var1 + var2
+    return render(request, 'result.html', {'result':result})
