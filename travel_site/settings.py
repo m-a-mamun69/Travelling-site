dd3c9b4d2ad320b1437e0d4d4ef4aeb8a3980c25
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-07-10 20:36:32 +0600

    Convert SQLite to Postgresql and Create One Model and Migrate.

--- travel_site/settings.py
+++ travel_site/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'travel_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travelsite',
+        'USER': 'postgres',
+        'PASSWORD': '0869',
+        'HOST': 'localhost'
     }
 }
 
