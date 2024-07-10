3f09c3bdd063050274a4e44c504c5fec1e275bb9
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-07-10 11:42:04 +0600

    How to Apply 'if/else Statment'.

--- travello/views.py
+++ travello/views.py
@@ -9,18 +9,21 @@ def index(request):
     dest1.desc = 'The City That Naver Sleeds'
     dest1.img = 'destination_1.jpg'
     dest1.price = 100
+    dest1.offer = False
 
     dest2 = Destination()
     dest2.name = 'Chattogram'
     dest2.desc = 'Chittagong is large port city on the southeastern coast of Bangladesh'
     dest2.img = 'destination_2.jpg'
     dest2.price = 150
+    dest2.offer = False
 
     dest3 = Destination()
     dest3.name = 'Rajshahi'
     dest3.desc = 'Rajshahi is a historic center of silk production And Clean City'
     dest3.img = 'destination_3.jpg'
     dest3.price = 120
+    dest3.offer = True
 
     dests = [dest1, dest2, dest3]
 
