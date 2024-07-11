0b6599258704ab64b1aef75633793aa3fb95d879
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-07-11 12:05:53 +0600

    Adding images And Fetch Datat Form Databese Get Things Up On The Page

--- travello/views.py
+++ travello/views.py
@@ -3,28 +3,14 @@ from .models import Destination
 # Create your views here.
 
 def index(request):
+    # dest3 = Destination()
+    # dest3.name = 'Rajshahi'
+    # dest3.desc = 'Rajshahi is a historic center of silk production And Clean City'
+    # dest3.img = 'destination_3.jpg'
+    # dest3.price = 120
+    # dest3.offer = True
+    # dests = [dest1, dest2, dest3]
 
-    dest1 = Destination()
-    dest1.name = 'Dhaka'
-    dest1.desc = 'The City That Naver Sleeds'
-    dest1.img = 'destination_1.jpg'
-    dest1.price = 100
-    dest1.offer = False
-
-    dest2 = Destination()
-    dest2.name = 'Chattogram'
-    dest2.desc = 'Chittagong is large port city on the southeastern coast of Bangladesh'
-    dest2.img = 'destination_2.jpg'
-    dest2.price = 150
-    dest2.offer = False
-
-    dest3 = Destination()
-    dest3.name = 'Rajshahi'
-    dest3.desc = 'Rajshahi is a historic center of silk production And Clean City'
-    dest3.img = 'destination_3.jpg'
-    dest3.price = 120
-    dest3.offer = True
-
-    dests = [dest1, dest2, dest3]
+    dests = Destination.objects.all()
 
     return render(request, 'index.html', {'dests':dests})
